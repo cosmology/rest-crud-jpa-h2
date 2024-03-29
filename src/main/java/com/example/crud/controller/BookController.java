2fbf51a64254ee59aad93069ed6ccc8b4bb6214d
Ivan Prokic <ivanprokic@gmail.com>
2024-03-29 12:07:58 -0700

    Code cleanup

--- src/main/java/com/example/crud/controller/BookController.java
+++ src/main/java/com/example/crud/controller/BookController.java
@@ -4,6 +4,7 @@ import com.example.crud.entity.Book;
 import com.example.crud.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,46 +25,35 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RequestMapping("/api/v1")
 public class BookController {
 
-    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+    @Autowired
+    private BookService bookService;
 
-    AtomicInteger atomicInteger = new AtomicInteger();
-
-    private final BookService bookService;
-
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @PostMapping("book")
+    @PostMapping("/book")
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
         return bookService.createBook(book);
     }
 
-    @GetMapping("book/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id) throws InterruptedException {
-        var response = bookService.getBookById(id);
-        var invokeCount = atomicInteger.incrementAndGet();
-        // Thread.sleep(3000);
-        log.info("count: {} response: {} on {}", invokeCount, response.getStatusCode(), Thread.currentThread());
+    @GetMapping("/book/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
-    @GetMapping("books")
+    @GetMapping("/books")
     public ResponseEntity<List<Book>> getAllBooks() {
         return bookService.getAllBooks();
     }
 
-    @PutMapping("book/{id}")
+    @PutMapping("/book/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
         return bookService.updateBook(id, book);
     }
 
-    @PatchMapping("book/{id}")
+    @PatchMapping("/book/{id}")
     public Book updatePartialBook(@PathVariable Long id, @RequestBody Map<String, Object> fields) {
         return bookService.updatePartialBook(id, fields);
     }
 
-    @DeleteMapping("book/{id}")
+    @DeleteMapping("/book/{id}")
     public ResponseEntity<HttpStatus> deleteBook(@PathVariable Long id) {
         return bookService.deleteBook(id);
     }
